02eac0b17e580852fa7801de8c3d3076f1ce9db0
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-01-29 16:51:53 -0500

    update 242, remove debugging output

--- Arrays & Hashing/NC_242.java
+++ Arrays & Hashing/NC_242.java
@@ -21,7 +21,7 @@ public class NC_242 {
     //     // return s_arr == t_arr; this is incorrect since it compares their hashCodes
     //     return Arrays.equals(t_arr, s_arr);
     // }
-    
+
     /* O(n) */
     public boolean isAnagram(String s, String t) {
         HashMap<Character, Integer> map = new HashMap<>();
@@ -29,13 +29,13 @@ public class NC_242 {
         //count freq. of characters in string s
         for(char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1); //defaults to zero if c is not in the hashmap. If it IS in the hashmap, then increment the value by one.
-            System.out.println(map);
+            // System.out.println(map);
         }
 
         //decrement freq. of characters in string t from map
         for(char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) - 1); //defaults to zero if c is not in the hashmap. If it IS in the hashmap, then decrement the value by one.
-            System.out.println(map);
+            // System.out.println(map);
         }
 
         //check if any character has non-zero frequency in the map
