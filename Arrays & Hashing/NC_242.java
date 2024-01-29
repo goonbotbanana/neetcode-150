f75e6ed1e2324f4f121140e551be063a5e4959e7
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-01-29 16:50:43 -0500

    update 242, add O(n) solution

--- Arrays & Hashing/NC_242.java
+++ Arrays & Hashing/NC_242.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class NC_242 {
     public static void main(String[] args) {
@@ -8,16 +9,40 @@ public class NC_242 {
         System.out.println(nc.isAnagram(s, t));
     }
     /* O(nlogn) */
+    // public boolean isAnagram(String s, String t) {
+    //     char[] s_arr = s.toCharArray();
+    //     char[] t_arr = t.toCharArray();
+
+    //     Arrays.sort(s_arr);
+    //     System.out.println("s string sorted: " +Arrays.toString(s_arr));
+    //     Arrays.sort(t_arr);
+    //     System.out.println("t string sorted: " +Arrays.toString(t_arr));
+
+    //     // return s_arr == t_arr; this is incorrect since it compares their hashCodes
+    //     return Arrays.equals(t_arr, s_arr);
+    // }
+    
+    /* O(n) */
     public boolean isAnagram(String s, String t) {
-        char[] s_arr = s.toCharArray();
-        char[] t_arr = t.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        //count freq. of characters in string s
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1); //defaults to zero if c is not in the hashmap. If it IS in the hashmap, then increment the value by one.
+            System.out.println(map);
+        }
 
-        Arrays.sort(s_arr);
-        System.out.println("s string sorted: " +Arrays.toString(s_arr));
-        Arrays.sort(t_arr);
-        System.out.println("t string sorted: " +Arrays.toString(t_arr));
+        //decrement freq. of characters in string t from map
+        for(char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1); //defaults to zero if c is not in the hashmap. If it IS in the hashmap, then decrement the value by one.
+            System.out.println(map);
+        }
 
-        // return s_arr == t_arr; this is incorrect since it compares their hashCodes
-        return Arrays.equals(t_arr, s_arr);
+        //check if any character has non-zero frequency in the map
+        for(int val : map.values()) {
+            if (val != 0) return false;
+        }
+
+        return true;
     }
 }
